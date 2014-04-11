957b7449219a45b1038f20637682aba28363a1dc
Dennis Weber <nijin22@gmail.com>
2014-04-11 18:47:41 +0200

    Adds testtins

--- play_framework/hello-play/app/controllers/MainController.java
+++ play_framework/hello-play/app/controllers/MainController.java
@@ -6,7 +6,11 @@ import play.mvc.Result;
 public class MainController extends Controller {
     
     public static Result index() {
-        return ok(views.html.index.render("Hello from Java"));
+        return ok(views.html.index.render("hallo"));
+    }
+    
+    public static Result neu() {
+        return ok(views.html.neu.render());
     }
     
 }
