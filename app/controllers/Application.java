784f712d5a3792d2c53056f7f79f72caabcd2ab3
Johannes Dangelmaier <e.mail-adresse@gmx.de>
2014-06-10 03:36:03 +0200

    adds list-refreshing via ajax + fixes bugs
    restore old ‚playing‘

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import model.java.Match;
 import model.java.MatchController;
 import model.java.Ship;
+import model.java.Map;
 import model.java.exceptions.ShipAlreadyAddedException;
 import model.java.exceptions.ShipNotPlacableException;
 import play.mvc.*;
@@ -12,12 +13,37 @@ import views.html.*;
 public class Application extends Controller{
 	private static HashMap<Integer, WebSocket.Out<String>> hostsWaitingForGuest = new HashMap<Integer, WebSocket.Out<String>>();
     
+    
+    // TODO: ---> loadling list replaced by ajax
     public static Result start() {
     	MatchController matchController = MatchController.getInstance();
     	HashMap<Integer, Match> openMatches = matchController.getOpenMatches();
     	//System.out.println(openMatches.toString());
     	return ok(start.render(openMatches));
     }
+    
+    // TODO: ---> replaced by ajax
+    public static Result refreshAvailableGames() {
+    	MatchController matchController = MatchController.getInstance();
+    	HashMap<Integer, Match> openMatches = matchController.getOpenMatches();
+    	//System.out.println(openMatches.toString());
+        
+        String result = "[";
+        
+        boolean firstTime = true;
+        for(Integer key : openMatches.keySet()){
+          result += "{\"key\" : " + key + ", \"hostname\" : " + "\"" + openMatches.get(key).getHost().getName() + "\""+ "},";
+        }
+        
+        // TODO: specify last element - "," Problem
+        result += "{\"key\" : 0 , \"hostname\" : 0}]";
+        
+        System.out.print(result);
+        
+    	return ok(result);
+    }
+    
+    
     
     public static Result joinGame(String username, String matchID){
     	MatchController matchController = MatchController.getInstance();
@@ -30,61 +56,70 @@ public class Application extends Controller{
 			//Game is not waiting for another player anymore
     		return redirect("/"); //Tell him to get back to homepage
 		}
-    	wsHost.write("OTHER PLAYER JOINED");
+    	wsHost.write(username);
     	wsHost.close(); //Close WebSocket
     	hostsWaitingForGuest.remove(new Integer(matchID)); //Remove host from list of webSockets
     	
     	session("matchID", matchID); //Save the unique match ID to a session cookie.
     	session("isHost", "false"); //To identify whether the user is host or guest, we set this session cookie.
-    	
-    	return redirect("/placeShips");
+        
+        Map host = match.getHost();
+        String hostname = host.getName();
+        
+        
+    	return redirect("/placeShips?user1=" + username + "&user2=" + hostname);
     }
     
-    public static Result placeShips(){
-    	return ok(placeShips.render());
+    public static Result placeShips(String user1, String user2){
+    	return ok(placeShips.render(user1,user2));
     }
     public static Result placeShipsFailed(){
     	return ok(placeShipsFailed.render());
     }
     
-    public static Result playing(String c_x, String c_y, String c_o, String b_x, String b_y, String b_o, String d1_x, String d1_y, String d1_o, String d2_x, String d2_y, String d2_o, String s1_x, String s1_y, String s1_o, String s2_x, String s2_y, String s2_o) {
-    	//find map
-    	MatchController matchController = MatchController.getInstance();
-    	Match match = matchController.getMatchByID(Integer.parseInt(session("matchID")));
-    	model.java.Map map;
-    	if (session("isHost") == "true") {
-			map = match.getHost();
-		} else {
-			map = match.getGuest();
-		}    	
-    	
-    	//place the ships
-    	try {
-    		Ship.Orientation orientation = (c_o == "h" ? Ship.Orientation.HORIZONTAL : Ship.Orientation.VERTICAL);
-			map.addShip(Ship.Type.CARRIER, Integer.parseInt(c_x), Integer.parseInt(c_y), orientation);
-			
-			orientation = (b_o == "h" ? Ship.Orientation.HORIZONTAL : Ship.Orientation.VERTICAL);
-			map.addShip(Ship.Type.CARRIER, Integer.parseInt(b_x), Integer.parseInt(b_y), orientation);
-			
-			orientation = (d1_o == "h" ? Ship.Orientation.HORIZONTAL : Ship.Orientation.VERTICAL);
-			map.addShip(Ship.Type.CARRIER, Integer.parseInt(d1_x), Integer.parseInt(d1_y), orientation);
-			
-			orientation = (d2_o == "h" ? Ship.Orientation.HORIZONTAL : Ship.Orientation.VERTICAL);
-			map.addShip(Ship.Type.CARRIER, Integer.parseInt(d2_x), Integer.parseInt(d2_y), orientation);
-			
-			orientation = (s1_o == "h" ? Ship.Orientation.HORIZONTAL : Ship.Orientation.VERTICAL);
-			map.addShip(Ship.Type.CARRIER, Integer.parseInt(s1_x), Integer.parseInt(s1_y), orientation);
-			
-			orientation = (s2_o == "h" ? Ship.Orientation.HORIZONTAL : Ship.Orientation.VERTICAL);
-			map.addShip(Ship.Type.CARRIER, Integer.parseInt(s2_x), Integer.parseInt(s2_y), orientation);
-			
-		} catch (ShipAlreadyAddedException | ShipNotPlacableException e) {
-			return redirect("/placeShipsFailed");
-		}
-    	
-    	//TODO;;;;;
-       return ok(playing.render());
+    public static Result playing(){
+      return ok(playing.render());
     }
+    
+    
+//    public static Result playing(String c_x, String c_y, String c_o, String b_x, String b_y, String b_o, String d1_x, String d1_y, String d1_o, String d2_x, String d2_y, String d2_o, String s1_x, String s1_y, String s1_o, String s2_x, String s2_y, String s2_o) {
+//    	//find map
+//    	MatchController matchController = MatchController.getInstance();
+//    	Match match = matchController.getMatchByID(Integer.parseInt(session("matchID")));
+//    	model.java.Map map;
+//    	if (session("isHost") == "true") {
+//			map = match.getHost();
+//		} else {
+//			map = match.getGuest();
+//		}    	
+//    	
+//    	//place the ships
+//    	try {
+//    		Ship.Orientation orientation = (c_o == "h" ? Ship.Orientation.HORIZONTAL : Ship.Orientation.VERTICAL);
+//			map.addShip(Ship.Type.CARRIER, Integer.parseInt(c_x), Integer.parseInt(c_y), orientation);
+//			
+//			orientation = (b_o == "h" ? Ship.Orientation.HORIZONTAL : Ship.Orientation.VERTICAL);
+//			map.addShip(Ship.Type.CARRIER, Integer.parseInt(b_x), Integer.parseInt(b_y), orientation);
+//			
+//			orientation = (d1_o == "h" ? Ship.Orientation.HORIZONTAL : Ship.Orientation.VERTICAL);
+//			map.addShip(Ship.Type.CARRIER, Integer.parseInt(d1_x), Integer.parseInt(d1_y), orientation);
+//			
+//			orientation = (d2_o == "h" ? Ship.Orientation.HORIZONTAL : Ship.Orientation.VERTICAL);
+//			map.addShip(Ship.Type.CARRIER, Integer.parseInt(d2_x), Integer.parseInt(d2_y), orientation);
+//			
+//			orientation = (s1_o == "h" ? Ship.Orientation.HORIZONTAL : Ship.Orientation.VERTICAL);
+//			map.addShip(Ship.Type.CARRIER, Integer.parseInt(s1_x), Integer.parseInt(s1_y), orientation);
+//			
+//			orientation = (s2_o == "h" ? Ship.Orientation.HORIZONTAL : Ship.Orientation.VERTICAL);
+//			map.addShip(Ship.Type.CARRIER, Integer.parseInt(s2_x), Integer.parseInt(s2_y), orientation);
+//			
+//		} catch (ShipAlreadyAddedException | ShipNotPlacableException e) {
+//			return redirect("/placeShipsFailed");
+//		}
+//    	
+//    	//TODO;;;;;
+//       return ok(playing.render());
+//    }
     
     public static Result createGame(String username){
     	MatchController matchController = MatchController.getInstance();
