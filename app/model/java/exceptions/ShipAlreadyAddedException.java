38130f24c95da358a82a1f39b8f8171adb673860
Dennis Weber <nijin22@gmail.com>
2014-06-19 21:27:20 +0200

    removes unneccesary files

--- app/model/java/exceptions/ShipAlreadyAddedException.java
+++
@@ -1,9 +0,0 @@
-package model.java.exceptions;
-
-public class ShipAlreadyAddedException extends WSDException {
-
-	public ShipAlreadyAddedException() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
