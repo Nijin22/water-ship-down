356d5d92b2fff7761846ad0f33f51c0b8aae61b5
Johannes Dangelmaier <e.mail-adresse@gmx.de>
2014-06-19 23:55:50 +0200

    adds header in playing-view

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -132,7 +132,18 @@ public class Application extends Controller{
     }
     
     public static Result playing(){
-      return ok(playing.render());
+        MatchController matchController = MatchController.getInstance();
+  		Match match = matchController.getMatchByID(Integer.parseInt(session("matchID")));
+  		String user1;
+  		String user2;
+  		if (session("isHost").equals("true")) {
+			user1 = match.getHost().getName();
+			user2 = match.getGuest().getName();
+		} else {
+			user2 = match.getHost().getName();
+			user1 = match.getGuest().getName();
+		}
+    	return ok(playing.render(user1,user2));
     }
     
     public static Result loadMaps(){
